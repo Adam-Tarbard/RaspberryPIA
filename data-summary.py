702f88b2cd5d938c333595333afc357d034b4c72
Adam-Tarbard <Adam-Tarbard@noreply.users.github.com>
2020-09-24 11:52:17 +0100

    Data Summary, dyanmodb working

--- data-summary.py
+++ data-summary.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 
-import boto3
 import json
+import boto3
 from boto3.dynamodb.conditions import Key
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('SensorData')
-
-client = boto3.resource('dynamodb')
-table = client.Table('SensorData')
-
-def lambda_handler(event, context):
-
-      response = table.query(
-        KeyConditionExpression=Key('SensorID').eq('Reading:RaspberryPiA') & Key('Timestamp').gt('2020-09-20')    
+def query_sensor_data(event, context):
+ dynamodb = boto3.resource('dynamodb')
+ 
+ table = dynamodb.Table('SensorData')
+ response = table.query(
+   KeyConditionExpression=Key("SensorID").eq("Reading:RaspberryPiA") & Key('Timestamp').gt('2020-09-24')    
         
-      )
-    
-      items = response['items']
-      for item in items:
-         print('items')
+ )
+ return response
 
+print(json.dumps(query_sensor_data('','')['Items'],indent=2))
 
