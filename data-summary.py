0960f4737271733e2bdd9d19fc3d35a597865940
Adam-Tarbard <Adam-Tarbard@noreply.users.github.com>
2020-09-24 14:12:58 +0100

    Data summary, time frame functional

--- data-summary.py
+++ data-summary.py
@@ -2,14 +2,17 @@
 
 import json
 import boto3
+import time
+from datetime import datetime, timedelta
 from boto3.dynamodb.conditions import Key
 
 def query_sensor_data(event, context):
  dynamodb = boto3.resource('dynamodb')
- 
+ now = datetime.now()
+ earlier = datetime.now() - timedelta(hours=5)
  table = dynamodb.Table('SensorData')
  response = table.query(
-   KeyConditionExpression=Key("SensorID").eq("Reading:RaspberryPiA") & Key('Timestamp').gt('2020-09-24')    
+   KeyConditionExpression=Key("SensorID").eq("Reading:RaspberryPiA") & Key('Timestamp').between(earlier.strftime("%Y-%m-%d %H:%M:%S"), now.strftime("%Y-%m-%d %H:%M:%S"))
         
  )
  return response
